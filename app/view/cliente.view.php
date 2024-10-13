214ecd46d7e8a3e439e0f3d0131cf8cfdc849dfc
Ruso312 <marcodace00@gmail.com>
2024-10-13 18:57:18 -0300

    Se crearon las primeras pre-visualizaciones en la vista de la aplicacion

--- app/view/cliente.view.php
+++ app/view/cliente.view.php
@@ -4,11 +4,25 @@ require_once "./model/cliente.model.php";
 require_once "./model/ejercicio.model.php";
 
 class ClienteEjercicioView{
-    private $view;
 
+    public function showEjercicios($ejercicios){
+        foreach($ejercicios as $ejercicio){
+            echo '<div>';
+                $ejercicio->nombre_ejercicio;
+                $ejercicio->musculo_implicado;
+                $ejercicio->descripcion;
+                $ejercicio->series;
+                $ejercicio->repeticiones;
+            echo '</div>';
+        }
+    }
 
-
-
-
+    public function showClientes($clientes){
+        foreach($clientes as $cliente){
+            echo '<div>';
+                $cliente->nombre;
+                $cliente->email;
+            echo '</div>';
+        }
+    }
 }
-
